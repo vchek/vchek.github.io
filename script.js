04351de678eeca194cb09795f9d8a0568da11ae0
Glitch (corporate-food-control) <none>
2019-04-18 22:13:54 +0000

    🍦🌝 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,9 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log('hi');
+
+$(function(){
+  $("#nav-placeholder").load("navbar.html");
+});
